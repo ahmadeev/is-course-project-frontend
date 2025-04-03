c76ec9829b4d523820d8cb2aae83dea83e115b97
danis <ahrnadeev@gmail.com>
2025-04-04 01:51:05 +0300

    Обновление стилей

--- src/components/_DBD/CharacterCard/CharacterCard.jsx
+++ src/components/_DBD/CharacterCard/CharacterCard.jsx
@@ -7,16 +7,18 @@ const CharacterCard = ({ character, imageUrl }) => {
 
     return (
         <div className={styles.card}>
-            {imageUrl && (
-                <img src={imageUrl} alt={name} className={styles.image} />
-            )}
-            <div className={styles.header}>
-                <h2 className={styles.title}>{name}</h2>
-                <p className={styles.id}>Character ID: {id}</p>
+            <div className={styles.container + " " + styles.centerer}>
+                {imageUrl && (
+                    <img src={imageUrl} alt={name} className={styles.image} />
+                )}
+                <div className={styles.header}>
+                    <h2 className={styles.title}>{name}</h2>
+                    <p className={styles.id}>Character ID: {id}</p>
+                </div>
+                <p className={styles.description}>
+                    {lore || 'История отсутствует'}
+                </p>
             </div>
-            <p className={styles.description}>
-                {lore || 'История отсутствует'}
-            </p>
             <div className={styles.perksSection}>
                 <h3 className={styles.perksTitle}>Перки</h3>
                 <PerkList data={perks} />
